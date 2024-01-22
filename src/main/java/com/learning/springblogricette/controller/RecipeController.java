12f8d8799d2d3b83c834acc47500355516e084fc
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-22 13:35:50 +0100

    milestone 1 - done

--- src/main/java/com/learning/springblogricette/controller/RecipeController.java
+++ src/main/java/com/learning/springblogricette/controller/RecipeController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 import java.util.Optional;
@@ -74,6 +75,18 @@ public class RecipeController {
             Recipe recipeToEdit = result.get();
             Recipe savedRecipe = recipeRepository.save(formRecipe);
             return "redirect:/recipes/show/" + id;
+        } else {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe with id " + id + " not found");
+        }
+    }
+
+    @PostMapping("/delete/{id}")
+    public String delete(@PathVariable Integer id, RedirectAttributes redirectAttributes) {
+        Optional<Recipe> result = recipeRepository.findById(id);
+        redirectAttributes.addFlashAttribute("redirectMessage", result.get().getTitle() + " recipe, deleted!");
+        if (result.isPresent()) {
+            recipeRepository.deleteById(id);
+            return "redirect:/recipes";
         } else {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Recipe with id " + id + " not found");
         }
