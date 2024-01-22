7c3219d5afc11c2158ce2f8563268a8f868132d4
GianmarcoOrabona <gianmarcoorabona091@gmail.com>
2024-01-22 15:13:52 +0100

    add create method and view for category

--- src/main/java/com/learning/springblogricette/controller/CategoryController.java
+++ src/main/java/com/learning/springblogricette/controller/CategoryController.java
@@ -2,10 +2,14 @@ package com.learning.springblogricette.controller;
 
 import com.learning.springblogricette.model.Category;
 import com.learning.springblogricette.repository.CategoryRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -22,5 +26,22 @@ public class CategoryController {
         List<Category> categoryList = categoryRepository.findAll();
         model.addAttribute("categoryList", categoryList);
         return "categories/list";
+    }
+
+    @GetMapping("/create")
+    public String create(Model model) {
+        Category category = new Category();
+        model.addAttribute("category", category);
+        return "categories/create";
+    }
+
+    @PostMapping("/create")
+    public String store(@Valid @ModelAttribute("category") Category formCategory, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "categories/create";
+        } else {
+            Category savedCategory = categoryRepository.save(formCategory);
+            return "redirect:/categories";
+        }
     }
 }
